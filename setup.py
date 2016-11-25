cff6c241d9362bb3325bad2318be9e5216d7cd1b
George Brighton <george@gebn.co.uk>
2016-11-25 19:13:33 +0000

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,40 @@
 # -*- coding: utf-8 -*-
+import codecs
 from setuptools import setup, find_packages
 
-import codecs
 
-with codecs.open('LICENSE') as f:
-    license_ = f.read()
+def _read_file(name, encoding='utf-8'):
+    """
+    Read the contents of a file.
+
+    :param name: The name of the file in the current directory.
+    :param encoding: The encoding of the file; defaults to utf-8.
+    :return: The contents of the file.
+    """
+    with codecs.open(name, encoding=encoding) as f:
+        return f.read()
+
 
 setup(
     name='chandl',
     version='0.0.1',
     description='A lightweight tool for parsing and downloading 4chan threads.',
-    license=license_,
+    long_description=_read_file('README.md'),
+    license='MIT',
     url='https://github.com/gebn/chandl',
     author='George Brighton',
     author_email='os@gebn.co.uk',
     packages=find_packages(),
     install_requires=[
         'six'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
     ]
 )
