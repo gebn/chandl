f09951c52ea124cacbc4be7c082173ed61a7b622
George Brighton <george@gebn.co.uk>
2017-02-14 08:36:44 +0000

    Start implementing tests for `__main__._parse_args()`

--- chandl/tests/test_main.py
+++ chandl/tests/test_main.py
@@ -4,7 +4,7 @@ import unittest
 import sys
 import six
 
-from chandl import __main__
+from chandl import __main__ as main
 
 
 class TestPrintError(unittest.TestCase):
@@ -14,7 +14,19 @@ class TestPrintError(unittest.TestCase):
     def test_stream(self):
         try:
             sys.stderr = six.StringIO()
-            __main__._print_error(self._MESSAGE)
+            main._print_error(self._MESSAGE)
             self.assertEquals(sys.stderr.getvalue(), self._MESSAGE + '\n')
         finally:
             sys.stderr = sys.__stderr__
+
+
+class TestParseArgs(unittest.TestCase):
+
+    _DUMMY_URL = 'https://boards.4chan.org/wg/thread/6851190'
+
+    def test_verbosity_missing(self):
+        self.assertEqual(main._parse_args([self._DUMMY_URL]).verbosity, 0)
+
+    def test_verbosity_count(self):
+        self.assertEqual(main._parse_args([self._DUMMY_URL, '-vvvv']).verbosity,
+                         4)
